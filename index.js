7ce708be4224295088a95bae7c9fee32e2bcc50e
rocio-valencia <rociovalencialusso@gmail.com>
2023-05-29 22:10:44 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document. querySelector ("button-menu-toggle");
+addEventListener ("click", function () {
+document. querySelector (".nav-links")
+classList. toggle ("nav-links-responsive")
+})
+
+
+
